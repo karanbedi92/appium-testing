2aa8f560bea079b4c880bae5c30ba90f30344f14
karan bedi <karanbedi792@gmail.com>
2018-02-13 10:22:21 +0530

    Appium testing first successful run for sign in

--- src/scenarios/AndroidDriverSetup.java
+++ src/scenarios/AndroidDriverSetup.java
@@ -1,5 +1,38 @@
 package scenarios;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+@SuppressWarnings("rawtypes")
 public class AndroidDriverSetup {
+	
+	protected AndroidDriver driver;
+
+    protected void prepareAndroidForAppium() throws MalformedURLException {
+    	//fetching the application (.apk) path
+		File classpathRoot = new File(System.getProperty("user.dir"));
+		File appDir = new File(classpathRoot, "/apps/");
+		File app = new File(appDir, "com.ebay.mobile_v5.17.0.18-117_Android-5.0.apk");
+		
+		//desired capabilities initialization for test execution
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability("deviceName", "Pixel");
+		capabilities.setCapability("platformVersion", "7.1.2");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("app", app.getAbsolutePath());
+		capabilities.setCapability("appPackage", "com.ebay.mobile");
+		capabilities.setCapability("appActivity", "com.ebay.mobile.activities.MainActivity");
+		
+		//appium server url
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+    }
 
 }
--- src/scenarios/AppiumTest.java
+++ src/scenarios/AppiumTest.java
@@ -1,9 +1,35 @@
 package scenarios;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
 
-public class AppiumTest {
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
+
+public class AppiumTest extends AndroidDriverSetup{
   @Test
-  public void f() {
+  public void signIn() throws InterruptedException {
+	 driver.findElement(By.id("com.ebay.mobile:id/home")).click();
+	 driver.findElement(By.id("com.ebay.mobile:id/home")).click();
+	 Thread.sleep(1000);
+	 driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).sendKeys("karanbedi792@gmail.com");
+	 driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).sendKeys("Jan@2018");
+	 driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+	 driver.findElement(By.id("com.ebay.mobile:id/button_google_deny")).click();
+	 Thread.sleep(1000);
   }
+  @BeforeTest
+  public void beforeTest() throws MalformedURLException {
+	  //initializing driver for android testing
+	  prepareAndroidForAppium();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  //quiting the driver after everything is done
+	  driver.quit();
+  }
+
 }
