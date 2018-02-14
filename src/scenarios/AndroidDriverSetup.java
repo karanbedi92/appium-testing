151f8035b03faf25c4fe0be9c909513f2707520f
karan bedi <karanbedi792@gmail.com>
2018-02-14 10:17:00 +0530

    Day 2 first commit. Reading from appium testing properties file done.

--- src/scenarios/AndroidDriverSetup.java
+++ src/scenarios/AndroidDriverSetup.java
@@ -3,6 +3,7 @@ package scenarios;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.CapabilityType;
@@ -10,29 +11,33 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import support_libraries.Setting;
 @SuppressWarnings("rawtypes")
 public class AndroidDriverSetup {
 	
 	protected AppiumDriver driver;
 
     protected void prepareAndroidForAppium() throws MalformedURLException {
+    	Setting appiumProperties=new Setting();
+    	System.out.println(appiumProperties.getInstance().getProperty("InstallApplicationInDevice"));
+    	
     	//fetching the application (.apk) path
 		File classpathRoot = new File(System.getProperty("user.dir"));
 		File appDir = new File(classpathRoot, "/apps/");
-		File app = new File(appDir, "com.ebay.mobile_v5.17.0.18-117_Android-5.0.apk");
+		File app = new File(appDir, appiumProperties.getInstance().getProperty("Android_APK_Name"));
 		
 		//desired capabilities initialization for test execution
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-		capabilities.setCapability("deviceName", "Pixel");
-		capabilities.setCapability("platformVersion", "7.1.2");
-		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("deviceName", appiumProperties.getInstance().getProperty("Device_Name"));
+		capabilities.setCapability("platformVersion", appiumProperties.getInstance().getProperty("Platform_Version"));
+		capabilities.setCapability("platformName", appiumProperties.getInstance().getProperty("platformVersion"));
 		capabilities.setCapability("app", app.getAbsolutePath());
-		capabilities.setCapability("appPackage", "com.ebay.mobile");
-		capabilities.setCapability("appActivity", "com.ebay.mobile.activities.MainActivity");
+		capabilities.setCapability("appPackage", appiumProperties.getInstance().getProperty("Application_Package_Name"));
+		capabilities.setCapability("appActivity", appiumProperties.getInstance().getProperty("Application_MainActivity_Name"));
 		
 		//appium server url
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver = new AndroidDriver(new URL(appiumProperties.getInstance().getProperty("AppiumURL")), capabilities);
 		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
     }
 
--- src/support_libraries/Setting.java
+++ src/support_libraries/Setting.java
@@ -1,5 +1,7 @@
 package support_libraries;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,22 +9,20 @@ import java.util.Properties;
 
 
 public class Setting {
-	
 	static Properties properties = new Properties();
-
 	public Setting(){
-
-	try {
-		InputStream inputStream = Setting.class.getClassLoader().getResourceAsStream("appium-testing.properties");
-		System.out.println(inputStream.toString());
-		properties.load(inputStream);
-	} catch (FileNotFoundException e) {
-		System.out.println("FileNotFoundException while loading the appium-testing properties file");
-	} catch (IOException e) {
-		System.out.println("IOException while loading the appium-testing properties file");
+		try {
+			String dir = System.getProperty("user.dir");
+	        InputStream inputStream = new FileInputStream(dir + "/appium-testing.properties");
+	        properties.load(inputStream);
+	        inputStream.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException while loading the appium-testing properties file");
+		} catch (IOException e) {
+			System.out.println("IOException while loading the appium-testing properties file");
+		}
 	}
-	}
-	public static Properties getInstance() {
+	public Properties getInstance() {
 		return properties;
 	}
 }
